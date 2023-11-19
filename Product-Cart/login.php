02dfc7186784e6cacb2101defc433ccb6f7529cc
NicholasWong13 <nicholaswong613@gmail.com>
2023-11-19 22:46:09 +0800

    Commit by Nicholas Wong

--- Product-Cart/login.php
+++ Product-Cart/login.php
@@ -1,18 +1,32 @@
-<?php 
+<?php
+// Set session cookie parameters for security
+$sessionParams = session_get_cookie_params();
+session_set_cookie_params(
+    $sessionParams["lifetime"],
+    $sessionParams["path"],
+    $sessionParams["domain"],
+    true, // Secure flag: Change to true if using HTTPS
+    true  // HTTPOnly flag
+);
+session_start();
+session_regenerate_id(true); // Regenerate session ID to prevent session fixation
 
-  session_start();
 
-  require 'assets/conn.php';
-  require 'functions.php';
+require 'assets/conn.php';
+require 'functions.php';
 
-  if(isset($_POST['login'])) {
-
+if (isset($_POST['login'])) {
     $uname = clean($_POST['username']);
     $pword = clean($_POST['password']);
 
-    $query = "SELECT * FROM users WHERE username = '$uname' AND password = '$pword'";
-
-    $result = mysqli_query($con, $query);
+    // Using prepared statements to prevent SQL injection
+    $query = "SELECT * FROM users WHERE username = ? AND password = ?";
+    $stmt = mysqli_prepare($con, $query);
+    
+    // Bind parameters and execute the query
+    mysqli_stmt_bind_param($stmt, 'ss', $uname, $pword);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
 
     if (mysqli_num_rows($result) > 0) {
         $row = mysqli_fetch_assoc($result);
@@ -31,82 +45,60 @@
         $_SESSION['errprompt'] = "Wrong username or password.";
     }
 }
-
-  if(!isset($_SESSION['username'], $_SESSION['password'])) {
-
+if (!isset($_SESSION['username'], $_SESSION['password'])) {
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<title>Login - Studio C Hair & Beauty Salon</title>
-
-	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/css/main.css" rel="stylesheet">
-
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login - Studio C Hair & Beauty Salon</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/main.css" rel="stylesheet">
 </head>
 <body>
-  
-  <?php include 'login-header.php'; ?>
-  
-  <section class="center-text"><br /><br /><br />
-    
-    <h3>User Login</h3>
+    <?php include 'login-header.php'; ?>
+    <section class="center-text"><br /><br /><br />
 
-    <div class="login-form box-center">
-      <?php 
-
-        if(isset($_SESSION['prompt'])) {
-          showPrompt();
-        }
-
-        if(isset($_SESSION['errprompt'])) {
-          showError();
-        }
-
-      ?>
-      <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
-        
-        <div class="form-group">
-          <label for="username" class="sr-only">Username</label>
-          <input type="text" class="form-control" name="username" placeholder="Username" required autofocus>
+        <h3>Login</h3>
+        <div class="login-form box-center">
+            <?php 
+            if(isset($_SESSION['prompt'])) {
+                showPrompt();
+            }
+            if(isset($_SESSION['errprompt'])) {
+                showError();
+            }
+            ?>
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+                <div class="form-group">
+                    <label for="username" class="sr-only">Username</label>
+                    <input type="text" class="form-control" name="username" placeholder="Username" required autofocus>
+                </div>
+                <div class="form-group">
+                    <label for="password" class="sr-only">Password</label>
+                    <input type="password" class="form-control" name="password" placeholder="Password" required>
+                </div>
+                <a href="register.php">Need an account?</a>
+                <input class="btn btn-primary" type="submit" name="login" value="Log In">
+                <br/><a href="http://localhost/6005CEM-Security/home.php">Back to Main Homepage</a>
+            </form>
         </div>
-
-        <div class="form-group">
-          <label for="password" class="sr-only">Password</label>
-          <input type="password" class="form-control" name="password" placeholder="Password" required>
-        </div>
-        
-        <a href="register.php">Need an account?</a>
-        <input class="btn btn-primary" type="submit" name="login" value="Log In">
-        <br/><a href="http://localhost/StudioCHair&BeautySalon/home.php">Back to Main Homepage</a>
-
-      </form>
-
-  </section>
-
-  </div>
-
-	<script src="assets/js/jquery-3.1.1.min.js"></script>
-	<script src="assets/js/bootstrap.min.js"></script>
+    </section>
+    <script src="assets/js/jquery-3.1.1.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
 </body>
 </html>
 
 <?php
-
-  } else {
+} else {
     header("location:profile.php");
     exit;
-  }
+}
 
-  unset($_SESSION['prompt']);
-  unset($_SESSION['errprompt']);
-
-  mysqli_close($con);
-
+unset($_SESSION['prompt']);
+unset($_SESSION['errprompt']);
+mysqli_close($con);
 ?>
